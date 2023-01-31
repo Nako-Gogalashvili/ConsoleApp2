7d479658faa9b171c2c7114ac18fd6941be9a23f
DELL <DELL@DESKTOP-H6BTBSN>
2023-01-31 05:13:36 +0400

    Add project files.

---
+++ ConsoleApp2/OOPbasis/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Students
+    {  
+       public string Name( get; set; )
+       public int Age(get; set;)
+       public string Faculty( get; set;)
+       public string Degree(get; set;)
+     }
+}  public Students (string Name, int Age, string Faculty, string Degree)
+{
+   this.Name = Name;
+   this.Age = Age;
+    this.Faculty = Faculty;
+    this.Degree = Degree;
+}
+
+  
+
+
+
+
+
+ 
+
+
---
+++ ConsoleApp2/OOPbasis/University.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class University
+    {
+        static void Main(string[] args)
+        {
+          List<Students> listofStudents = new List<Students>();
+          listofStudents.Add(new Students("Keti", 21, "Mathematic", 'Bs'));
+          listofStudents.Add(new Students("Natia", 23, "History", "Bs"));
+          listofStudents.Add(new Students("Beqa", 25, "Biology", "Ms"));
+          listofStudents.Add(new Students("Sandro", 22, "Geography", "Bs"));
+          listofStudents.Add(new Students("Giorgi", 35, "History", "Ms"));
+          listofStudents.Add(new Students("Salome", 28, "Mathemathic", "Ms"));
+
+           var sortedstudent = from student in listofStudents
+                                where student.Faculty == "History"
+                                select student;
+
+            foreach (Students elements in sortedstudent)
+            Console.WriteLine(elements.ToString());
+
+           var student = from student in listofStudents
+                         where student.Age >= 28 && Degree == "Ms"
+                         select student;
+
+           var number = from number in listofStudents
+                        where number => 22
+                        orderby number descending
+                        select number;
+
+           foreach (Students elem in number)
+            Console.WriteLine(elem.ToString());
+
+
+
+    }
+
+
+
+    }
+}
